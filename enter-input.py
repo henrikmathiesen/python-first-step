c5582735735535df42ac451ce8301f3637e506f8
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-10-20 17:44:55 +0200

    -just testing stuff

---
+++ enter-input.py
@@ -0,0 +1,7 @@
+print("Enter your name:")
+name = input()
+
+print("Enter your age:")
+age = input()
+
+print("You entered: " + name + " " + age)
---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print("Hello, world")
+
+x = "some string"
+
+# This is a comment
+
+if 5 > 2:
+    print("5 is greater than 2 " + x)
+
+def myFunction(v):
+    print("I made a function " + v)
+
+myFunction("yeah!");
